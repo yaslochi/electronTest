50f98bdc7f0ac501ebf351a107ba60545ef1438d
yasmin-lozano <yasmin.lozano@laureate.net>
2020-01-15 10:26:20 -0600

    Create auto-updating app

---
+++ main.js
@@ -0,0 +1,54 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+//for updates
+const { autoUpdater } = require('electron-updater')
+
+let mainWindow;
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  mainWindow.loadFile('index.html');
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', () => {
+    //for updates
+    autoUpdater.checkForUpdatesAndNotify();
+    createWindow();
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on('app_version', (event) => {
+  event.sender.send('app_version', { version: app.getVersion() });
+});
+
+ipcMain.on('restart_app', () => {
+    autoUpdater.quitAndInstall();
+});
+
+//for updates
+  autoUpdater.on('update-available', () => {
+    mainWindow.webContents.send('update_available');
+  });
+
+  autoUpdater.on('update-downloaded', () => {
+    mainWindow.webContents.send('update_downloaded');
+  });
